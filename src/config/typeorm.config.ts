d47a3e54e54c77e0ef5f9f5f4f020f825d72d000
uncleseneca <uncleseneca@gmail.com>
2020-05-15 10:05:23 +0300

    add configuration to typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,18 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
 import { Task } from 'src/tasks/task.entity';
 import { User } from 'src/auth/user.entity';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanagement',
-  entities: [Task, User],
-  synchronize: true,
-};
-
 export const typeOrmErrorCodes = {
   DUPLICATE_ENTITY: '23505',
+};
+
+const dbConfig = config.get('db');
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  entities: [Task, User],
+  synchronize: dbConfig.synchronize,
 };
